fb99e51555f159013326d046a63dd049c096e56e
ybourassi-rgb <ybourassi@fibersav.com>
2025-11-23 21:17:45 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,19 +3,16 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      // Dealabs + CDN
       { protocol: "https", hostname: "www.dealabs.com" },
+      { protocol: "https", hostname: "static-pepper.dealabs.com" },
       { protocol: "https", hostname: "static-hotukdeals.akamaized.net" },
-      { protocol: "https", hostname: "**.dealabs.com" },
+      { protocol: "https", hostname: "*.dealabs.com" },
 
-      // Amazon images (souvent .media-amazon / images-eu)
       { protocol: "https", hostname: "**.amazon.com" },
       { protocol: "https", hostname: "**.amazon.fr" },
-      { protocol: "https", hostname: "**.media-amazon.com" },
-
-      // autres classiques
       { protocol: "https", hostname: "i.imgur.com" },
       { protocol: "https", hostname: "img.leboncoin.fr" },
+      // ajoute au fur et à mesure les domaines vus dans tes feeds
     ],
   },
 };
