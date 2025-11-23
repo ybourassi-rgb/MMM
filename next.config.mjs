3641a616343f354d14a11011c6d11fff2ab3f24e
ybourassi-rgb <ybourassi@fibersav.com>
2025-11-23 20:55:13 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,17 @@
 // next.config.mjs
-/** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
   images: {
     remotePatterns: [
-      // Dealabs / HotUKDeals CDNs
       { protocol: "https", hostname: "www.dealabs.com" },
       { protocol: "https", hostname: "static-hotukdeals.akamaized.net" },
-      { protocol: "https", hostname: "images.dealabs.com" },
-      { protocol: "https", hostname: "images-hotukdeals.akamaized.net" },
-      { protocol: "https", hostname: "static-pepper.dealabs.com" },
-
-      // Amazon / Ali / Ebay / etc.
-      { protocol: "https", hostname: "m.media-amazon.com" },
-      { protocol: "https", hostname: "images-eu.ssl-images-amazon.com" },
+      { protocol: "https", hostname: "*.dealabs.com" },
+      { protocol: "https", hostname: "*.amazon.com" },
+      { protocol: "https", hostname: "*.amazon.fr" },
       { protocol: "https", hostname: "i.imgur.com" },
       { protocol: "https", hostname: "img.leboncoin.fr" },
-      { protocol: "https", hostname: "ae01.alicdn.com" },
-      { protocol: "https", hostname: "i.ebayimg.com" },
+      // ajoute au fur et à mesure les domaines vus dans tes feeds
     ],
   },
 };
