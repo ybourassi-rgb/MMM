7d9c13ce6e5ee80279294f48ed911ba40a28afbe
ybourassi-rgb <ybourassi@fibersav.com>
2025-11-25 04:57:21 +0100

    Create page.jsx

---
+++ app/publier/enchere/page.jsx
@@ -0,0 +1,9 @@
+{
+  "title": "Carte Pokémon Dracaufeu 1st edition",
+  "description": "Très bon état, holo.",
+  "images": ["https://...jpg"],
+  "category": "loisirs",
+  "startPrice": 50,
+  "minIncrement": 5,
+  "durationHours": 48
+}
